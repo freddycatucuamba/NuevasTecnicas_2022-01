e19f0a5e87dc9c6d33f0e6d57112ee5ff92b326c
Carlos Guagrilla <carlos.guagrilla@hotmail.com>
2022-01-17 21:18:40 -0500

    comit

---
+++ hello.js
@@ -0,0 +1,47 @@
+/*
+/*var num1 = 1;
+let num2 = 2;
+const num3 = 3;
+
+console.log(num1);
+*/
+
+/*const num1111 = 4;
+const num11111 = 5;*/
+/*const suma = num + num1;*/
+/*console.log(suma)*/
+
+/*function sumar (num, num1){
+    return num + num1
+}*/
+
+
+
+/*const res = sumar(8,5);*/
+/*console.log(res);*/
+
+/*const persona = {
+nombre: "Carlos",
+apellido:"Guagrilla",
+edad: 20    
+}*/
+/*console.log(persona)*/
+
+
+
+
+console.WriteLine("ingrese un numero");
+num =console.ReadLine();
+
+console.WriteLine("ingrese un numero");
+num1 =console.ReadLine();
+
+function mayor (num, num1){
+    if (num > num1){
+        console.log("el numero: ", num)
+    }
+    if (num1 > num){
+        console.log("el numero: ", num1)
+    }
+}
+console.log(mayor);
