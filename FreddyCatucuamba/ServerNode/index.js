8fd910a967aa9ab6be605d3d9d2b338fa066337c
Catucuamba <xavox@msn.com>
2022-01-24 20:47:37 -0500

    Express, middleware, y params

--- FreddyCatucuamba/ServerNode/index.js
+++ FreddyCatucuamba/ServerNode/index.js
@@ -13,7 +13,7 @@ const funcionServidor = function(req,res){
 const server = http.createServer(funcionServidor);
 server.listen(3000, ()=>{console.log("Servidor escuchando el puerto 3000".red)});
 */
-
+/*
 const express = require('express');
 const server = express();
 
@@ -52,6 +52,32 @@ server.delete('/eliminarUsuario', function(req,res){
 server.listen(3000, function(){
     console.log("Servidor en el puerto 300");
 })
+*/
+
+/////////////////////////////////////////////////
+
+const expres = require('express');
+const morgan = require('morgan');
+const app = expres();
+
+function logger(req, res, next){
+    console.log(`Petición recibida: ${req.protocol}://${req.get('host')}${req.originalUrl}`)
+    next();
+}
+app.use(expres.json());
+app.use(logger);
+app.use(morgan('tiny'));
+
+app.set('port',4000);
+
+app.get('/conParametro/:id',(req,res)=>{
+    console.log(req.params)
+    res.send("<h1> Con parámetros funcionando </h1>")
+})
+
+app.listen(app.get('port'), (req, res)=>{
+    console.log("El servidor está en el puerto", app.get('port'))
+})
 
 
 //----------------------
@@ -73,3 +99,43 @@ server.listen(3000, function(){
 });
 */
 //------
+/*
+/////////MIDDLEWARES//////////////////////
+const express = require('express');
+const { Server } = require('http');
+const { hostname } = require('os');
+const morgan = require('morgan');
+const server = express();
+
+function logger(req, res, next){
+    console.log("Petición recibida");
+    next();
+}
+function logger2(req, res, next){
+    console.log(`Petición recibida: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+}
+server.use(express.json());
+server.use(logger);
+server.use(logger2);
+server.use(morgan('combined'));
+
+
+
+server.get('/', function(req,res){
+    res.json({
+        nombre:"Freddy",
+        apellido:"Catucuamba",
+        edad:38
+    });   
+})
+
+server.listen(3000, function(){
+    console.log("Servidor en el puerto 3000");
+})
+
+
+//////////////////////EXPRESS SETTINGS
+
+
+*/
